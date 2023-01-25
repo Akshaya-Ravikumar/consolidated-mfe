8b81f4c33d009cbb7cb9d9249b7e6d19685c0153
Akshaya <rakshayaee@gmail.com>
2023-01-25 16:32:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react';
+import './App.css';
+
+const ReactMFE2 = React.lazy(
+  () => import ('mfe2/Mfe2Index')
+)
+
+function App() {
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+          <ReactMFE2 />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
---
+++ bootstrap.js
@@ -0,0 +1,6 @@
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import('mfe1/Mfe1Index')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
